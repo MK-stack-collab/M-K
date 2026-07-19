cb487167ba134a27b8e477880088ce31385f1373
MK <michaelfortune343@gmail.com>
2026-07-19 20:58:14 +0100

    michael

--- index/script.js
+++ index/script.js
@@ -1,4 +1,21 @@
-let a=co;
-let b=go;
-let c=a;
-console.log(`output: ${c}`);
+const menu = document.getElementById("menu");
+const navLinks = document.getElementById("navLinks");
+const icon = menu.querySelector("i");
+
+menu.onclick = function(){
+
+    navLinks.classList.toggle("active");
+
+    if(navLinks.classList.contains("active")){
+
+        icon.classList.remove("fa-bars");
+        icon.classList.add("fa-xmark");
+
+    }else{
+
+        icon.classList.remove("fa-xmark");
+        icon.classList.add("fa-bars");
+
+    }
+
+}
